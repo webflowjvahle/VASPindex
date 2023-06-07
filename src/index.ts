3eaf228329abaabd0c83183bd08706d7a69c1fb6
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2023-06-07 22:22:27 +0200

    Added getzomshift to window resize

--- src/index.ts
+++ src/index.ts
@@ -120,6 +120,7 @@ function init3D() {
   window.addEventListener('resize', () => {
     renderer.setSize(parentElement1.clientWidth, parentElement1.clientHeight);
     camera.aspect = parentElement1.clientWidth / parentElement1.clientHeight;
+    camera.position.set(0, 0.5, getzoomshift());
     camera.updateProjectionMatrix();
   });
 
